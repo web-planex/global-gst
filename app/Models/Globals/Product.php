9c99299a44aa70fc404b6eb39d6f60fab15eeb88
Mukund <mukundh@webplanex.com>
2021-01-13 17:39:00 +0530

    unit add in product

--- app/Models/Globals/Product.php
+++ app/Models/Globals/Product.php
@@ -22,6 +22,15 @@ class Product extends Model
     const UNIT_L = 'L';
     const UNIT_PIECE = 'PIECE';
     const UNIT_QT = 'QT';
+    const UNIT_BOX = 'BOX';
+    const UNIT_CM = 'CM';
+    const UNIT_DZ = 'DZ';
+    const UNIT_FT = 'FT';
+    const UNIT_IN = 'IN';
+    const UNIT_KM = 'KM';
+    const UNIT_LB = 'LB';
+    const UNIT_MG = 'MG';
+    const UNIT_M = 'M';
 
     public static $unit = [
         self::UNIT_KG => 'KG',
@@ -30,6 +39,15 @@ class Product extends Model
         self::UNIT_L => 'L',
         self::UNIT_PIECE => 'PIECE',
         self::UNIT_QT => 'QT',
+        self::UNIT_BOX => 'BOX',
+        self::UNIT_CM => 'CM',
+        self::UNIT_DZ => 'DZ',
+        self::UNIT_FT => 'FT',
+        self::UNIT_IN => 'IN',
+        self::UNIT_KM => 'KM',
+        self::UNIT_LB => 'LB',
+        self::UNIT_MG => 'MG',
+        self::UNIT_M => 'M',
     ];
 
     public function User(){
